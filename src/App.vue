e34130debf755fa01d278e4b4ec6b814fe0105b7
deni5n <denisne1ru@gmail.com>
2020-11-04 13:50:19 +0500

    init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,118 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <ul>
+      <li v-for="user of users" :key="user.id">
+        {{ user.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
+<script>
+export default {
+  name: 'app',
+  computed: {
+    users() {
+      return this.$store.state.user;
+    }
+  },
+  async mounted() {
+    try {
+      await this.$store.dispatch('init','user');
+      // do something, all is ok...
+    } catch (error) {
+      // show an error, or something to do ...
+      console.log('app: ' + error);
+    }
+  }
+};
+
+
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  padding: 0;
+  margin: 0;
+  border: 0;
 }
 
-#nav {
-  padding: 30px;
+*, *:before, *:after {
+  -moz-box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+:focus, :active {
+  outline: none;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+a:focus, a:active {
+  outline: none;
+}
+
+nav, footer, header, aside {
+  display: block;
+}
+
+html, body {
+  height: 100%;
+  width: 100%;
+  font-size: 100%;
+  line-height: 1;
+  font-size: 14px;
+  -ms-text-size-adjust: 100%;
+  -moz-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+}
+
+input, button, textarea {
+  font-family: inherit;
+}
+
+input::-ms-clear {
+  display: none;
+}
+
+button {
+  cursor: pointer;
+}
+
+button::-moz-focus-inner {
+  padding: 0;
+  border: 0;
+}
+
+a, a:visited {
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: none;
+}
+
+ul li {
+  list-style: none;
+}
+
+img {
+  vertical-align: top;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  font-size: inherit;
+  font-weight: 400;
+}
+
+body{
+  background-color: #f0f0f0;
+}
+
+#app{
+  background-color: #fff;
+  padding: 2rem;
+  width: 972px;
+  margin: 0 auto;
+  height: 100%;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,30 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+      state: {
+          user: []
+      },
+      mutations: {
+          init(state, payload) {
+              state.user = payload;
+          }
+      },
+      actions: {
+          init({commit}, endpoint) {
+              return new Promise(async(res, rej) => {
+                  try {
+                      const {data} = await axios.get(`https://5fa25f72ba0736001613b895.mockapi.io/${endpoint}`);
+                      commit('init', data);
+                      res(true);
+                  } catch (error) {
+                      rej(false);
+                  }
+              });
+          }
+      },
+      modules: {}
+  });
